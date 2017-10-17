0a08c4147ca8b664ca1291462eeed50d7df65602
Marvin-LOUIS <marvinlouis@idea-box.tech>
2017-10-17 17:11:55 +0200

    Exercices 12

--- Exercice-11/app.js
+++ Exercice-11/app.js
@@ -1,2 +1,5 @@
 // Exercice 11
+var data = new XMLHttpRequest();
 
+data.open('get','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json',true);
+data.send();
---
+++ Exercice-12/app.js
@@ -0,0 +1,16 @@
+// Exercice 01
+var hover = function (event) {
+  console.log("Il est vivant!!!!!");
+  element.style.backgroundColor = "red";
+}
+
+var element = document.getElementsByClassName("hoverMe");
+for (let I=0;I<element.length;I++){
+  element[I].addEventListener("click",hover);
+}
+
+// Exercice 02
+
+
+// Exercice 03
+
